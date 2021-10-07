591c70afc62bb2ba841650ede82cf099d1979460
Max <loncarmax@gmail.com>
2021-10-07 17:25:48 +0200

    Second commit - added SASS, fixed slider, etc.

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,47 @@
-$(function() {
+'use strict';
+
+$(document).ready(function () {
     var topRow = $('.top_row');
     var botRow = $('.bottom_row');
 
-    $('.arrow_right').on('click', function() {
-        // console.log('right:');
-        // console.log($('.top_slide').last().width());
-        topRow.animate({left: $('.top_slide').last().width()}, 500, function() {
+    var slideRight = function slideRight() {
+        $('.arrow_right').removeClass('active');
+        topRow.animate({ left: $('.top_slide').last().width() + 10 }, 800, function () {
             topRow.css('left', '0px');
             $('.top_slide').first().before($('.top_slide').last());
+            $('.arrow_right').addClass('active');
         });
-        // console.log($('.bot_slide').last().width());
-        botRow.animate({left: $('.bot_slide').last().width()}, 500, function() {
+        botRow.animate({ left: $('.bot_slide').last().width() + 10 }, 800, function () {
             botRow.css('left', '0px');
             $('.bot_slide').first().before($('.bot_slide').last());
         });
+    };
+
+    var slideLeft = function slideLeft() {
+        $('.arrow_left').removeClass('active');
+        var top_item_width = $('.top_slide').first().width() + 10;
+        var bot_item_width = $('.bot_slide').first().width() + 10;
+
+        $('.top_slide').last().after($('.top_slide').first());
+        topRow.css({ 'left': top_item_width + 'px' });
+        topRow.animate({ 'left': 0 }, 800, function () {
+            $('.arrow_left').addClass('active');
+        });
+
+        $('.bot_slide').last().after($('.bot_slide').first());
+        botRow.css({ 'left': bot_item_width + 'px' });
+        botRow.animate({ 'left': 0 }, 800);
+    };
+
+    $('.arrow_right').on('click', function () {
+        if ($('.arrow_right').hasClass('active')) {
+            slideRight();
+        }
     });
 
-    $('.arrow_left').on('click', function() {
-        // console.log('left:');
-        // console.log($('.top_slide').first().width());
-        topRow.animate({right: $('.top_slide').first().width()}, 500, function() {
-            topRow.css('right', '0px');
-            $('.top_slide').last().after($('.top_slide').first());
-        });
-        // console.log($('.bot_slide').first().width());
-        botRow.animate({right: $('.bot_slide').first().width()}, 500, function() {
-            botRow.css('right', '0px');
-            $('.bot_slide').last().after($('.bot_slide').first());
-        });
+    $('.arrow_left').on('click', function () {
+        if ($('.arrow_left').hasClass('active')) {
+            slideLeft();
+        }
     });
-
-})
+});
---
+++ src/main.js
@@ -0,0 +1,46 @@
+$(document).ready(() => {
+    let topRow = $('.top_row');
+    let botRow = $('.bottom_row');
+
+    let slideRight = () => {
+        $('.arrow_right').removeClass('active');
+        topRow.animate({left: $('.top_slide').last().width() + 10}, 800, () => {
+            topRow.css('left', '0px');
+            $('.top_slide').first().before($('.top_slide').last());
+            $('.arrow_right').addClass('active');
+        });
+        botRow.animate({left: $('.bot_slide').last().width() + 10}, 800, () => {
+            botRow.css('left', '0px');
+            $('.bot_slide').first().before($('.bot_slide').last());
+        });
+    }
+
+    let slideLeft = () => {
+        $('.arrow_left').removeClass('active');
+        let top_item_width = $('.top_slide').first().width() + 10;
+        let bot_item_width = $('.bot_slide').first().width() + 10;  
+
+        $('.top_slide').last().after($('.top_slide').first());  
+        topRow.css({'left' : (top_item_width) + 'px'}); 
+        topRow.animate({'left' : 0}, 800, () => {
+            $('.arrow_left').addClass('active');
+        });
+
+        $('.bot_slide').last().after($('.bot_slide').first());  
+        botRow.css({'left' : (bot_item_width) + 'px'}); 
+        botRow.animate({'left' : 0}, 800);
+};
+
+    $('.arrow_right').on('click', () => {
+        if ($('.arrow_right').hasClass('active')) {
+            slideRight();
+        }
+    });
+
+    $('.arrow_left').on('click', () => {
+        if ($('.arrow_left').hasClass('active')) {
+            slideLeft();
+        }
+    });
+
+});
